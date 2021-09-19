31bfbbe2daed95dcd1a2b7e01a0688d32e50ec35
Luis Felipe <luisfelipesdn12@gmail.com>
2021-09-19 13:03:30 -0300

    feat: Try to get packages

---
+++ exercises/ConsoleTesteBanco/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+
+namespace ConsoleTesteBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MySqlConnection conn = new MySqlConnection("server=localhost;user id=root;password=root;database=teste");
+        }
+    }
+}
