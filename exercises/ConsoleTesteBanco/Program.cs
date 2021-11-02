73106fbc30d87bd1b62f630961b329d374f89950
Luis Felipe <luisfelipesdn12@gmail.com>
2021-11-02 17:04:13 -0300

    feat: Test database

--- exercises/ConsoleTesteBanco/Program.cs
+++ exercises/ConsoleTesteBanco/Program.cs
@@ -7,8 +7,31 @@ namespace ConsoleTesteBanco
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            MySqlConnection conn = new MySqlConnection("server=localhost;user id=root;password=root;database=teste");
+            const string connectionString = "server=localhost;user id=luis;password=[passs];database=[database]";
+
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            Console.WriteLine("Conectando ao banco de dados...");
+            
+            try
+            {
+                conn.Open();
+
+                var command = conn.CreateCommand();
+                command.CommandText = "SELECT * FROM product";
+
+                var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Console.WriteLine($"{reader["id"]} - {reader["name"]} - {reader["price"]}");
+                }
+
+                Console.WriteLine("Conectado!");
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Erro: " + ex.Message);
+            }
         }
     }
 }
