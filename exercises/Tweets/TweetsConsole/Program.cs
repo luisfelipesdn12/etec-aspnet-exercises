bcbdb8e79e235eff1015a0d85ce0266a0c11d126
Luis Felipe <luisfelipesdn12@gmail.com>
2021-11-02 23:47:37 -0300

    ✨ feat: Add console CRUD

--- exercises/Tweets/TweetsConsole/Program.cs
+++ exercises/Tweets/TweetsConsole/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using TweetsModels;
+using PromptCLI;
+using System.Collections.Generic;
 
 namespace TweetsConsole
 {
@@ -7,19 +9,100 @@ namespace TweetsConsole
     {
         static void Main(string[] args)
         {
-            var tweet = new Tweet{
-                Id = 1,
-                Text = "Hello World!",
-                CreatedAt = DateTime.Now,
-                User = "luisfelipesdn12",
-                LikesCount = 0,
-                RetweetsCount = 0,
-                RepliesCount = 0
-            };
+            while (true) {
+                string option = GetMenuOption();
 
-            Console.WriteLine(tweet.Text);
+                switch (option)
+                {
+                    case "Create Tweet":
+                        CreateTweet();
+                        break;
+                    case "Read Tweets":
+                        ReadTweets();
+                        break;
+                    case "Update Tweet":
+                        UpdateTweet();
+                        break;
+                    case "Delete Tweet":
+                        DeleteTweet();
+                        break;
+                    case "Stop":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
 
+        static string GetMenuOption()
+        {
+            string option = "";
+
+            var prompt = new Prompt();
+
+            prompt.Add(new SelectComponent<string>(
+                    "Select what you want to do:",
+                    new List<string>() {
+                        "Create Tweet", "Read Tweets", "Update Tweet", "Delete Tweet", "Stop"
+                    }
+                ))
+                .Callback(v => option = v);
+            prompt.Begin();
+
+            return option;
+        }
+
+        static Tweet GetTweetOption() {
+            Tweet tweet = null;
+            var prompt = new Prompt();
+
+            prompt.Add(new SelectComponent<Tweet>(
+                    "Select the tweet you want to update:",
+                    Tweet.GetAll()
+                ))
+                .Callback(t => tweet = t);
+
+            prompt.Begin();
+
+            return tweet;
+        }
+
+        static void CreateTweet()
+        {
+            var prompt = new Prompt();
+            Tweet tweet = prompt.Run<Tweet>();
+
+            tweet.Text = tweet.Text.Trim();
+            tweet.User = tweet.User.Trim();
             tweet.Save();
+        }
+
+        static void ReadTweets()
+        {
+            var tweets = Tweet.GetAll();
+            foreach (var tweet in tweets) {
+                tweet.Display();
+            }
+
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+        }
+
+        static void UpdateTweet() {
+            var tweet = GetTweetOption();
+
+            var prompt = new Prompt();
+                prompt
+                    .Add(new InputComponent("Tweet: ", tweet.Text))
+                    .Callback(text => tweet.Update(text.Trim()));
+
+            prompt.Begin();
+        }
+
+        static void DeleteTweet() {
+            var tweet = GetTweetOption();
+            tweet.Delete();
         }
     }
 }
--- exercises/Tweets/TweetsModels/Tweet.cs
+++ exercises/Tweets/TweetsModels/Tweet.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using PromptCLI;
 
 namespace TweetsModels
 {
@@ -8,23 +10,124 @@ namespace TweetsModels
         [Display(Name = "Tweet ID")]
         public int Id { get; set; }
 
-        [Display(Name = "Tweet Text")]
+        [Display(Name = "Tweet Text"), Input("Tweet:")]
         public string Text { get; set; }
 
-        [Display(Name = "Usename")]
+        [Display(Name = "Usename"), Input("Username:")]
         public string User { get; set; }
 
         [Display(Name = "Tweeted At")]
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
 
         [Display(Name = "Likes")]
-        public int LikesCount { get; set; }
+        public int LikesCount { get; set; } = 0;
 
         [Display(Name = "Retweets")]
-        public int RetweetsCount { get; set; }
+        public int RetweetsCount { get; set; } = 0;
 
         [Display(Name = "Replies")]
-        public int RepliesCount { get; set; }
+        public int RepliesCount { get; set; } = 0;
+
+        public override string ToString()  {
+            return $"{Id} - {Text} (@{User})";
+        }
+
+        // Display tweet
+        public void Display()
+        {
+            string lastLine = $"{RetweetsCount} Retweets - {LikesCount} Likes - {RepliesCount} Respostas";
+
+            var textLines = new List<string>();
+
+            for (int start = 0; start < Text.Length; start += lastLine.Length)
+            {
+                var end = lastLine.Length;
+                if (end > Text.Length) end = Text.Length;
+
+                try {
+                    var textLine = Text.Substring(start, end);
+                    textLines.Add(textLine);
+                } catch (Exception) {
+                    var textLine = Text.Substring(start);
+                    textLines.Add(textLine);
+                }
+            }
+
+            string result = "";
+
+            result += $"{Id}\n";
+            result += $"=={new string('=', lastLine.Length + 2)}==\n";
+            result += $"|| @{User}{new string(' ', lastLine.Length - User.Length - 1)} ||\n";
+            result += $"|| {new string(' ', lastLine.Length)} ||\n";
+
+            foreach (var textLine in textLines)
+            {
+                result += $"|| {textLine}{new string(' ', lastLine.Length - textLine.Length)} ||\n";
+            }
+
+            result += $"|| {new string(' ', lastLine.Length)} ||\n";
+            result += $"|| {lastLine} ||\n";
+            result += $"=={new string('=', lastLine.Length + 2)}==\n";
+
+            Console.WriteLine(result);
+        }
+
+        // Get all tweets
+        public static List<Tweet> GetAll()
+        {
+            var conn = Database.GetConnection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM tweet";
+            var reader = cmd.ExecuteReader();
+
+            var tweets = new List<Tweet>();
+
+            while (reader.Read())
+            {
+                var tweet = new Tweet
+                {
+                    Id = reader.GetInt32(0),
+                    Text = reader.GetString(1),
+                    User = reader.GetString(2),
+                    CreatedAt = reader.GetDateTime(3),
+                    LikesCount = reader.GetInt32(4),
+                    RetweetsCount = reader.GetInt32(5),
+                    RepliesCount = reader.GetInt32(6)
+                };
+
+                tweets.Add(tweet);
+            }
+
+            conn.Close();
+            return tweets;
+        }
+    
+        public void Update(string newTweetText)
+        {
+            var conn = Database.GetConnection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "UPDATE tweet SET text = @text WHERE id = @id";
+            cmd.Parameters.AddWithValue("@text", newTweetText);
+            cmd.Parameters.AddWithValue("@id", Id);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void Delete()
+        {
+            var conn = Database.GetConnection();
+            conn.Open();
+
+            var cmd = conn.CreateCommand();
+            cmd.CommandText = "DELETE FROM tweet WHERE id = @id";
+            cmd.Parameters.AddWithValue("@id", Id);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
 
         // Save tweet on database
         public void Save()
